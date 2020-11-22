1021fbc26ea274aaee0e03c36b45c56395c4b0fb
quaintt <htwerner01@gmail.com>
2020-11-22 14:39:27 -0600

    Added comments

--- ioReq.cpp
+++ ioReq.cpp
@@ -1,3 +1,14 @@
+/*
+file: ioReq.cpp
+author: Gherkin
+modification history: 
+    Gherkin
+    November 22nd, 2020
+procedures:
+    ioReq - Constructor method
+    ioReq - Overloaded constructor that defines the request's track and sector values
+*/
+
 class ioReq
 {
 public:
@@ -6,10 +17,26 @@ public:
     int track, sector;
 };
 
+/*
+    ioReq()
+    author: Gherkin
+    date: Nov 22, 2020
+    description: Basic constructor method that only exists because I needed to create a temp object
+        in my quicksort partition methods.
+*/
 ioReq::ioReq()
 {
 }
 
+/*
+    ioReq(t, s)
+    author: Gherkin
+    date: Nov 22, 2020
+    description: Overloaded constructor that defines the request's track and sector values
+    parameters:
+        t  I/P  int  Track value of I/O request
+        s  I/P  int  Sector value of I/O request
+*/
 ioReq::ioReq(int t, int s)
 {
     this->track = t;
